431aeeb647079f1163261a7837ee8456dd2b74c5
vishvjeettanwar1623 <sbvj727@gmail.com>
2026-07-21 14:04:09 +0530

    Add React class examples with Vite setup and hooks demo

---
+++ react/class-3/03basicreactvite/src/App.jsx
@@ -0,0 +1,18 @@
+import DummyCompo from "./DummyCompo"
+
+function App() {
+  return (
+    
+    <div>
+      <DummyCompo />
+      <hr />
+      <h1>This is a Test Website</h1>
+      <p>This is another tag</p>
+      <hr />
+    </div>
+    
+  )
+}
+
+
+export default App
