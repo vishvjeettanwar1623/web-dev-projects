da98d9dd7e6d1a5b9edd6b8ee76e91f78a47fa03
vishvjeettanwar1623 <sbvj727@gmail.com>
2026-07-22 13:50:32 +0530

    Add React Router crash course app and styles

---
+++ react/react-router-crash-course/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h4 className='text-3xl bg-blue-300 text-center p-3 m-2'>React Router</h4>
+    </>
+  )
+}
+
+export default App
